43e9605e09cc619b481c9ad9347b97fc7a5fe8b5
ABjelotomic <anjinimejl@gmail.com>
2026-02-16 16:20:01 +0100

    debug true

--- megawatts_ninja/settings.py
+++ megawatts_ninja/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ldssl2svqff&hlho5$v^561b+)9tmq3-*figu-65+d0403&sx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '192.168.8.39',      # Your Pi's local IP
